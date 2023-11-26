d17232c22933088995d2d410d25bbd2ecbb91138
nweligalla <nayanaweligalla@gmail.com>
2023-11-26 17:28:25 +0530

    added Student entity and repos

---
+++ src/main/java/com/nweligalla/jpalifecycledemo/entities/Student.java
@@ -0,0 +1,72 @@
+package com.nweligalla.jpalifecycledemo.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    private LocalDateTime accountCreatedTime;
+    private LocalDateTime accountLastUpdatedTime;
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // getters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // getters
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getAccountCreatedTime() {
+        return accountCreatedTime;
+    }
+
+    public LocalDateTime getAccountLastUpdatedTime() {
+        return accountLastUpdatedTime;
+    }
+
+}
---
+++ src/main/java/com/nweligalla/jpalifecycledemo/repos/StudentRepo.java
@@ -0,0 +1,11 @@
+package com.nweligalla.jpalifecycledemo.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nweligalla.jpalifecycledemo.entities.Student;
+
+public interface StudentRepo extends JpaRepository<Student, Long> {
+
+
+    
+}
