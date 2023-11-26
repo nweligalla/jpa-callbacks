e74bf48bf6845ca432cb4b048d82115469480d5f
nweligalla <nayanaweligalla@gmail.com>
2023-11-26 17:09:11 +0530

    added dependencies

---
+++ src/main/java/com/nweligalla/jpalifecycledemo/JpaLifecycleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nweligalla.jpalifecycledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLifecycleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaLifecycleDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nweligalla/jpalifecycledemo/JpaLifecycleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nweligalla.jpalifecycledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLifecycleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
